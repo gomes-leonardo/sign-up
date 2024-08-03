90c80bc9eb5e91a69a50dee3c5f8f4ac3d42eeb2
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2024-08-03 14:07:59 -0300

    fix: jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -2,16 +2,15 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  transformIgnorePatterns: ['/node_modules/'],
-  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
-  coverageDirectory: '<rootDir>/coverage'
+  transformIgnorePatterns: ['/node_modules/']
 }
 
 export default config

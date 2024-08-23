6e065b1d26c8e89416003e7a9fdfb4599e40a11b
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2024-08-23 06:34:55 -0300

    feat: ensure AccountMongoRepository returns an account on soccess

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,16 +1,17 @@
 import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    return await new Promise(resolve => {
-      resolve({
-        id: 'any_id',
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      })
-    })
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account: AccountModel = {
+      id: result.insertedId.toString(),
+      ...accountData
+    }
+    console.log(account)
+    return account
   }
 }

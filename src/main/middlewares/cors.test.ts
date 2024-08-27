ec2ea387ef8f26fd422c6d26d597232462e2b23f
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2024-08-27 06:25:34 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,13 +1,13 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('CORS middleware', () => {
+describe('CORS Middleware', () => {
   test('Should enable CORS', async () => {
     app.get('/test_cors', (req, res) => {
       res.send()
     })
     await request(app)
-      .get('/test_corsr')
+      .get('/test_cors')
       .expect('access-control-allow-origin', '*')
   })
 })
